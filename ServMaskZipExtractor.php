0ca5c0a86d1c5d28a7f313aea0ab8ce0811789aa
Borislav Angelov <borislav.angelov@gmail.com>
2015-04-05 17:57:47 +0300

    Add extractor

--- ServMaskZipExtractor.php
+++ ServMaskZipExtractor.php
@@ -32,47 +32,68 @@ class ServMaskZipExtractor
 			throw new Exception('Unable to seek in zip file.');
 		}
 
-		// Read next 46 bytes
-		$data = @fread($this->zipFileHandler, 46);
-
-		// Central dir structure
-		$structure = @unpack('Vid/vversion/vversionExtracted/vflag/vcompression/vmtime/vmdate/Vcrc/VcompressedSize/Vsize/vfileNameLength/vextraLength/vcommentLength/vdisk/vinternal/Vexternal/Voffset', $data);
-		if ($structure === false) {
-			throw new Exception('Unable to unpack zip file.');
-		}
-
-		// Verify signature
-		if ($structure['id'] !== 0x02014b50) {
-			throw new Exception('Invalid signature of zip file. Make sure zip file is created by All in One WP Migration.');
-		}
-
-		// Get file name
-		if (($fileNameLength = $structure['fileNameLength'])) {
-			$structure['fileName'] = fread($this->zipFileHandler, $fileNameLength);
-		} else {
-			$structure['fileName'] = null;
-		}
-
-		// Get extra
-		if (($extraLength = $structure['extraLength'])) {
-			$structure['extra'] = fread($this->zipFileHandler, $extraLength);
-		} else {
-			$structure['extra'] = null;
-		}
-
-		// Get comment
-		if (($commentLength = $structure['commentLength'])) {
-			$structure['comment'] = fread($this->zipFileHandler, $commentLength);
-		} else {
-			$structure['comment'] = null;
-		}
-
-		var_dump($structure);
+		$entries = array();
 
 		// Loop over zip entries
 		for ($i = 0; $i < $endStructure['entries']; $i++) {
 
+			// Read next 46 bytes
+			$data = @fread($this->zipFileHandler, 46);
+
+			// Central directory structure
+			$structure = @unpack('Vid/vversion/vversionExtracted/vflag/vcompression/vmtime/vmdate/Vcrc/VcompressedSize/Vsize/vfileNameLength/vextraLength/vcommentLength/vdisk/vinternal/Vexternal/Voffset', $data);
+			if ($structure === false) {
+				throw new Exception('Unable to unpack zip file.');
+			}
+
+			// Verify signature
+			if ($structure['id'] !== 0x02014b50) {
+				throw new Exception('Invalid signature of zip file. Make sure zip file is created by All in One WP Migration.');
+			}
+
+			// Verify compression
+			if ($structure['compression'] !== 0) {
+				throw new Exception('Unsupported compression method. Make sure zip file is created by All in One WP Migration.');
+			}
+
+			// Verify general purpuse big flag
+			if (($structure['flag'] & 1) === 1) {
+				throw new Exception('Unsupported encryption method. Make sure zip file is created by All in One WP Migration.');
+			}
+
+			// Get file name
+			if (($fileNameLength = $structure['fileNameLength'])) {
+				$structure['fileName'] = fread($this->zipFileHandler, $fileNameLength);
+			} else {
+				$structure['fileName'] = null;
+			}
+
+			// Get extra
+			if (($extraLength = $structure['extraLength'])) {
+				$structure['extra'] = fread($this->zipFileHandler, $extraLength);
+			} else {
+				$structure['extra'] = null;
+			}
+
+			// Get comment
+			if (($commentLength = $structure['commentLength'])) {
+				$structure['comment'] = fread($this->zipFileHandler, $commentLength);
+			} else {
+				$structure['comment'] = null;
+			}
+
+			// Get compression
+
+			// // ----- Look if it is a directory
+			// if (substr($p_header['filename'], -1) == '/') {
+			//   //$p_header['external'] = 0x41FF0010;
+			//   $p_header['external'] = 0x00000010;
+			// }
+
+			$entries[] = $structure;
 		}
+
+		return $entries;
 	}
 
 	public function readEndOfCentralDirectory() {
